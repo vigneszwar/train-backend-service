69f7c08f0abec923d97c04e204372111209dea29
GIRISH <naveenutthra27@gmail.com>
2023-07-23 16:00:07 +0530

    Comitted by Navaneethan

--- src/main/java/com/example/demo/model/Stations.java
+++ src/main/java/com/example/demo/model/Stations.java
@@ -5,7 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
-//helloo
+//hello
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
