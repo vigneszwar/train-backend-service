2e334dfbe41a572fa0997c847daa982f95484a95
Mahavishnu G <stoicvishnu@gmail.com>
2023-07-23 15:29:30 +0530

    Vishnu commited

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,21 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int user_id;
+    private String name;
+    private String email;
+    private String pw;
+}
